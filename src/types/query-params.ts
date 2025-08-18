651ded6b6f1490bd69b5e4f31517c84402e1b3a4
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-08-18 13:53:55 +0600

    update: query params

--- src/types/query-params.ts
+++ src/types/query-params.ts
@@ -2,13 +2,20 @@ export interface CreateUserQueryParams {
   loginAfterCreate?: boolean;
 }
 
+export type DateFilter = "today" | "thisWeek" | "thisMonth";
+
 export interface IQuery {
   role?: string;
   isAffiliate?: boolean;
   isActive?: boolean;
   status?: string;
   search?: string;
+  searchFields?: string[];
+  filter?: Record<string, any>;
+  sort?: string;
   type?: string;
   page?: number;
   limit?: number;
+  dateFilter?: DateFilter;
+  includeStats?: boolean | "true";
 }
